96bb81bb7f35926c10341e23d4490710d0fd7614
Calvin Kopitz <calvinjk@umiGotmilk21ch.edu>
2015-12-27 18:47:56 -0500

    Added test scene, fixed movement issues.  Uses velocity instead of position now.

--- Tanks/Assets/_Scripts/TankMovement.cs
+++ Tanks/Assets/_Scripts/TankMovement.cs
@@ -6,18 +6,17 @@ public class TankMovement : MonoBehaviour {
     public float horizontalInput = 0f;
     public float verticalInput = 0f;
     public float playerSpeed = 1f;
-    public Vector3 playerPos;
 
 	// Use this for initialization
 	void Start () {
-        playerPos = transform.position;
+        
 	}
 
     void FixedUpdate() {
         horizontalInput = Input.GetAxis("Horizontal") * playerSpeed;
         verticalInput = Input.GetAxis("Vertical") * playerSpeed;
 
-        playerPos = new Vector3(playerPos.x + horizontalInput, playerPos.y, playerPos.z + verticalInput);
-        transform.position = playerPos;
+        Vector3 newVelocity = new Vector3(horizontalInput, 0, verticalInput);
+        GetComponent<Rigidbody>().velocity = newVelocity;
     }
 }
