12ce0236c5eb01f261a17d637a38dbf976ae42bc
Calvin Kopitz <calvinjk@umiGotmilk21ch.edu>
2015-12-27 17:51:58 -0500

    Added basic tank movement

---
+++ Tanks/Assets/_Scripts/TankMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour {
+
+    public float horizontalInput = 0f;
+    public float verticalInput = 0f;
+    public float playerSpeed = 1f;
+    public Vector3 playerPos;
+
+	// Use this for initialization
+	void Start () {
+        playerPos = transform.position;
+	}
+
+    void FixedUpdate() {
+        horizontalInput = Input.GetAxis("Horizontal") * playerSpeed;
+        verticalInput = Input.GetAxis("Vertical") * playerSpeed;
+
+        playerPos = new Vector3(playerPos.x + horizontalInput, playerPos.y, playerPos.z + verticalInput);
+        transform.position = playerPos;
+    }
+}
