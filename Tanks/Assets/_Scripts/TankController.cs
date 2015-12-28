fd39f643ce7d9db793dd88b0535a97f15a27a8e0
Calvin Kopitz <calvinjk@umiGotmilk21ch.edu>
2015-12-27 21:47:35 -0500

    Split Tank movement to two players

--- Tanks/Assets/_Scripts/TankController.cs
+++ Tanks/Assets/_Scripts/TankController.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class TankController : MonoBehaviour {
 
-    public float        accelerationInput   = 0f;
-    public float        turningInput        = 0f;
+    public int          playerNum           = 0;
     public float        moveSpeed           = 1f;
     public float        turnSpeed           = 1f;
     public float        bulletSpeed         = 1f;
     public GameObject   bulletPrefab;
-    public bool         ________________;
+    public bool         _________________________________;
+    public float        accelerationInput   = 0f;
+    public float        turningInput        = 0f;
     public Vector3      pos;
     public Vector3      rotation;  
     public GameObject   bullet;
@@ -20,12 +21,50 @@ public class TankController : MonoBehaviour {
 	}
 
     void FixedUpdate() {
-        //Update variables
+        //Update and reset variables
         pos = transform.position;
 
-        //Get input
-        turningInput = Input.GetAxis("Horizontal") * turnSpeed;
-        accelerationInput = Input.GetAxis("Vertical") * moveSpeed;
+        //I am upset that I have to do this, but this is the fix for now.
+        if (turningInput < -1)      { turningInput = -1; }
+        if (turningInput > 1)       { turningInput = 1; }
+        if (accelerationInput < -1) { accelerationInput = -1; }
+        if (accelerationInput > 1)  { accelerationInput = 1; }
+
+        //Check Player and get inputs
+        switch (playerNum) {
+            case 1:
+                //Turning input
+                if (Input.GetKeyDown(KeyCode.A))    { turningInput -= 1; }
+                if (Input.GetKeyDown(KeyCode.D))    { turningInput += 1; }
+                if (Input.GetKeyUp(KeyCode.A))      { turningInput += 1; }
+                if (Input.GetKeyUp(KeyCode.D))      { turningInput -= 1; }
+
+                //Acceleration input
+                if (Input.GetKeyDown(KeyCode.S))    { accelerationInput -= 1; }
+                if (Input.GetKeyDown(KeyCode.W))    { accelerationInput += 1; }
+                if (Input.GetKeyUp(KeyCode.S))      { accelerationInput += 1; }
+                if (Input.GetKeyUp(KeyCode.W))      { accelerationInput -= 1; }
+                break;
+            case 2:
+                if (Input.GetKeyDown(KeyCode.LeftArrow))    { turningInput -= 1; }
+                if (Input.GetKeyDown(KeyCode.RightArrow))   { turningInput += 1; }
+                if (Input.GetKeyUp(KeyCode.LeftArrow))      { turningInput += 1; }
+                if (Input.GetKeyUp(KeyCode.RightArrow))     { turningInput -= 1; }
+
+                //Acceleration input
+                if (Input.GetKeyDown(KeyCode.DownArrow))    { accelerationInput -= 1; }
+                if (Input.GetKeyDown(KeyCode.UpArrow))      { accelerationInput += 1; }
+                if (Input.GetKeyUp(KeyCode.DownArrow))      { accelerationInput += 1; }
+                if (Input.GetKeyUp(KeyCode.UpArrow))        { accelerationInput -= 1; }
+                break;
+            default:
+                print("INVALID PLAYERNUM");
+                break;
+        }
+
+        //Scale the inputs
+        turningInput *= turnSpeed;
+        accelerationInput *= moveSpeed;
 
         //Rotation stuff
         rotation = transform.localRotation.eulerAngles;
@@ -37,9 +76,8 @@ public class TankController : MonoBehaviour {
         GetComponent<Rigidbody>().velocity = newVelocity;
 
         //Shoot shit!
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter)) {
             bullet = Instantiate(bulletPrefab, pos + transform.forward.normalized, Quaternion.identity) as GameObject;
-
             bullet.GetComponent<Rigidbody>().velocity = transform.forward.normalized * bulletSpeed;
         }
     }
