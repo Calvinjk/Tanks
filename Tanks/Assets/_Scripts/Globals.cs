607671840d911a4c8c603699763cf8ac458eccea
Calvin Kopitz <calvinjk@umiGotmilk21ch.edu>
2016-01-07 11:41:30 -0500

    Added a global variables object for score tracking.  Renamed plane to floor, organized walls into a parent object

---
+++ Tanks/Assets/_Scripts/Globals.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : MonoBehaviour {
+
+    public bool ______________;
+
+    public static Globals S;
+
+    void Awake() {
+        S = this;
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
